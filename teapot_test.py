5eb2cb0105edaabce8345fd2a2c852b9bb76a13c
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-09-15 11:39:14 -0400

    joe over

--- teapot_test.py
+++ teapot_test.py
@@ -2,6 +2,16 @@ import wireframe
 import projection
 import shapes
 
+def initial(viewer: projection.ProjectionViewer):
+    viewer.translate_all([400, 298, 0])
+    viewer.scale_all(85)
+    viewer.rotate_all("x", -3)
+
 viewer = projection.ProjectionViewer()
-viewer.add_wireframe(wireframe.load_obj("teapot.obj"), "teapot")
+viewer.add_wireframe(wireframe.load_obj("teapot.obj"), "teapot", scale=85)
+viewer.show_nodes = False
+
+
+
+
 viewer.run()
